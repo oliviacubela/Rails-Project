5b7827d07ae3aa206078b98672f7fe7ac73f555d
Olivia Cubela <oliviacubela@gmail.com>
2021-01-14 22:18:17 -0500

    create a new dream, and added strong params

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,2 +1,21 @@
 class DreamsController < ApplicationController
+  def new
+    @dream = Dream.new
+    @dream.build_theme
+  end
+
+  def create
+    @dream = Dream.new(dream_params)
+    if @dream.save
+      redirect_to dream_path(@dream)
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def dream_params 
+    params.require(:dream).permit(:title, :content, :theme_id, theme_attributes: [:name, :description])
+  end
 end
