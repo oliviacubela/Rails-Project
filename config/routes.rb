dff60b4b76df69ab1f03f5907cbb703bf2829551
Olivia Cubela <oliviacubela@gmail.com>
2021-01-15 16:21:33 -0500

    nested routes for reactions

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
 
   #dont need post '/signup' bc we already have a route for it in resources :users
   resources :reactions
-  resources :dreams
+  resources :dreams do
+    resources :reactions, only: [:new, :index]
+  end
   resources :themes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
