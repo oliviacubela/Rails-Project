105a872288db4257c8e907ccf567a4a5773aa310
Olivia Cubela <oliviacubela@gmail.com>
2021-01-14 19:09:38 -0500

    added username validation & secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :dream_reactions, through: :reactions, source: :dream
   has_many :dreams
 
+  validates :username, uniqueness: true, presence: true
+
   has_secure_password
 end
 
