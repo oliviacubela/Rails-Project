10ba82964f80dffd1f4244fd5f21cb2af920c2bb
Olivia Cubela <oliviacubela@gmail.com>
2021-01-14 20:45:46 -0500

    create method for user login w/ authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController 
   def welcome 
   end
+
+  def destroy 
+    session.delete(:user_id)
+    redirect_to '/'
+  end
+
+  def new 
+  end
+
+  def create 
+    @user = User.find_by(username: params[:user][:username])
+    if @user.try(:authenticate, params[:user][:password])
+    # if @user && @user.authenticate(password: params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Login incorrect - please try again."
+      redirect_to login_path
+    end
+  end
 end
