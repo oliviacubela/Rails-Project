d6d64aad88cba73dfd1079f360b872b91056766d
Olivia Cubela <oliviacubela@gmail.com>
2021-01-15 16:20:58 -0500

    defined new reaction

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,2 +1,12 @@
 class ReactionsController < ApplicationController
+  
+  def new
+    @dream = Dream.find_by_id(params[:dream_id])
+    @reaction = @dream.reactions.build
+  end
+
+  def index 
+
+  end
+
 end
