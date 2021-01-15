19ccef7e4f6e4daf76381c094f4590152b87433f
Olivia Cubela <oliviacubela@gmail.com>
2021-01-15 16:20:11 -0500

    index and show for dreams

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -6,11 +6,20 @@ class DreamsController < ApplicationController
 
   def create
     @dream = Dream.new(dream_params)
+    @dream.user_id = session[:user_id]
     if @dream.save
       redirect_to dream_path(@dream)
     else
       render :new
     end
+  end
+
+  def index
+    @dreams = Dream.all 
+  end
+
+  def show 
+    @dream = Dream.find_by_id(params[:id])
   end
 
   private 
