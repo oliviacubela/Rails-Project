3e382ea2582c0ea965cf822ac26afc72a4e00386
Olivia Cubela <oliviacubela@gmail.com>
2021-01-17 13:23:45 -0500

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   end
 end
 
-#wrote the current_user method with find_by_id because it doesnt error out, rather returns nul if doesnt exist
+#wrote the current_user method with find_by_id because it doesnt error out, rather returns null if doesnt exist
 #use the 'or equals' bc it will only call the database once 
 #if you use the current_user method more than once in a method (learned this in sinatra)
