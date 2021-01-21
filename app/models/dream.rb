91876ea2a1d9426195a08c6ad70889658a8df28c
Olivia Cubela <oliviacubela@gmail.com>
2021-01-20 20:25:31 -0500

    validation to ensure user adds or creates theme

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -4,18 +4,20 @@ class Dream < ApplicationRecord
   has_many :reactions 
   has_many :users, through: :reactions
 
-  accepts_nested_attributes_for :theme
-
   validates :title, presence: true
   validates :content, presence: true
+
+  accepts_nested_attributes_for :theme, reject_if: proc { |attributes| attributes['name'].blank? }
+
+
 
   # def self.alpha 
   #   order(:title)
   # end
 
   # def theme_attributes=(attributes)
-    # binding.pry
-    # theme = Theme.find_or_create_by(attributes) if !attributes['name'].empty?
+  #   # binding.pry
+  #   theme = Theme.find_or_create_by(attributes) if !attributes['name'].empty?
   # end
   #can i change this up to serve as a validation? where if the attributes are empty you create it, or if they match by name then you send error message with "theme name already exists"
   #has to be a simpler way... uniqueness validation didnt work but I might've done scope wrong
