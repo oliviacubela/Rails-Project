d1d5f7f3d0eba31b29bf2954afba5afa4bd84dcc
Olivia Cubela <oliviacubela@gmail.com>
2021-01-20 20:25:53 -0500

    validate uniqueness of theme name

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -4,5 +4,11 @@ class Theme < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   
+  validates_uniqueness_of :name
+
+  #the validates_uniqueness_of works for new themes and preventing duplicates, but not for collection_select
+  #do i need to add scope? another method?
   #validates :name, uniqueness: { scope: :theme, message: "already exists - please select from drop-down" } 
 end
+
+
